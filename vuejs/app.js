176f8f672f2f5a11d94862fbfe23f34bef968f78
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-06-10 11:54:01 +0200

    first steps with vuejs

---
+++ vuejs/app.js
@@ -0,0 +1,16 @@
+new Vue({
+
+    el: '#app',
+    data: {
+        myValue: 42,
+    },
+
+    methods: {
+        add: function () {
+            this.myValue++;
+        },
+        subtract: function () {
+            this.myValue--;
+        }
+        }
+});
